0e19d685223046ee7361b91d69291a5279cf25e3
ijoanet <jramos@thehotelsnetwork.com>
2022-09-01 20:56:31 +0200

    feat(iteration 2): pass tweets as props to TweetComponent

---
+++ src/components/TweetComponent.vue
@@ -0,0 +1,59 @@
+<template>
+  <div className="tweet">
+    <img
+      :src="user.image"
+      className="profile"
+      alt="profile"
+    >
+
+    <div className="body">
+      <div className="top">
+        <span className="user">
+          <span className="name">{{ user.name }}</span>
+          <span className="handle">@{{ user.handle }}</span>
+        </span>
+
+        <span className="timestamp">{{ timestamp }}</span>
+      </div>
+
+      <p className="message">
+        {{ message }}
+      </p>
+
+      <div className="actions">
+        <!-- Font Awesome icons -->
+        <i class="far fa-comment" />
+        <i class="fas fa-retweet" />
+        <i class="far fa-heart" />
+        <i class="fas fa-share" />
+      </div>
+    </div>
+
+    <i class="fas fa-ellipsis-h" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+    timestamp: {
+      type: String,
+      default: 'Just now',
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
